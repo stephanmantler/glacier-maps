d237844e78035ce03ec4ca1833a5d50bbabcf310
stephan mantler <step@stephanmantler.com>
2020-04-19 10:54:19 +0000

    minor code cleaup

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Repository for server-side front end code running on [map.hafjall.is](http://map
 
 This is a simple single-page website presenting a comparison view of the glacier maps I am creating in Southeast Iceland.
 Primarily based on [OpenLayers](https://openlayers.org).
+
+I am also tracking the mapserver/mapcache configuration files in this repository but they are possibly worthless for
+others unless you're interested in seeing how they are structured.
--- main.js
+++ main.js
@@ -70,7 +70,7 @@ import('ol').then(_ => {
     for(var k in overlays[og]) {
       var ogl = overlays[og][k];
       var source = makeSource(ogl["layername"]);
-      source.setAttributions('glacier maps © <a href="http://www.hafjall.is/">Háfjall ehf.</a>');
+      source.setAttributions('glacier maps © <a href="http://www.hafjall.is/">Háfjall ehf.</a>. <a href="https://github.com/stephanmantler/glacier-maps/">Source on GitHub</a>.');
       var layer = new Tile({ opacity: 1, source: source, type: 'base', title: ogl["title"], visible:false, extent: ogl["extent"]});
       // lazy way to remember the very last layer we've added
       activeLayer = layer;
