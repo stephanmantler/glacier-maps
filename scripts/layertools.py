19dbb5bb7d8c18f5bf9cabfd227bb628a622f975
stephan mantler <step@stephanmantler.com>
2024-09-14 20:37:18 +0000

    upped cache resolution

--- scripts/layertools.py
+++ scripts/layertools.py
@@ -12,39 +12,39 @@ def readLayerDefs(tiledirs):
       print("## skipping " + tiledir)
       continue
     shapefile = shapes[0]
-    
+
     driver = ogr.GetDriverByName("ESRI Shapefile")
     dataSource = driver.Open(shapefile, 0)
     layer = dataSource.GetLayer()
     spatialRef = layer.GetSpatialRef()
     extent = layer.GetExtent()
     authority = spatialRef.GetAuthorityCode(None)
-    
+
     if authority != '3857':
       print("💥 unexpected authority code: " + authority + ", skipping " + tiledir)
       continue
-      
+
     layers.append([ tiledir, extent, shapefile ])
   return layers
 
 def writeCombinedMap(layers):
   print("🌐 writing combined.map ...")
-  
+
   mapfile = open("/var/www/is.icecaves.map/mapfiles/combined.map", "w")
-  
+
   mapfile.write("""MAP
   NAME "maps.stepman.is"
   STATUS ON
   SIZE 1000 1000
   #EXTENT 439300 7109000 440100 7110200
-  
+
   IMAGETYPE png
   UNITS DD
-  
+
   PROJECTION
     "init=epsg:3857"
   END #PROJECTION
-  
+
   WEB
     IMAGEPATH "/var/www/is.icecaves.map/map_tmp"
     IMAGEURL "/map_tmp/"
@@ -55,12 +55,12 @@ def writeCombinedMap(layers):
     END #METADATA
   END #WEB
   """)
-  
+
   for layer in layers:
     tiledir = layer[0]
     extent = layer[1]
     shapefile = layer[2]
-  
+
     mapfile.write("\n")
     mapfile.write("  ## from " + tiledir + " (via regen.py)\n")
     mapfile.write("  LAYER\n")
@@ -75,14 +75,14 @@ def writeCombinedMap(layers):
     mapfile.write("      'init=epsg:3857'\n")
     mapfile.write("    END\n")
     mapfile.write("  END #LAYER\n")
-  
+
   mapfile.write("END #MAP\n")
   mapfile.close()
-  
+
   print("🚛 writing cache config ...")
-  
+
   cachefile = open("/var/www/is.icecaves.map/mapcache/mapcache.yaml", "w")
-  
+
   cachefile.write("""services:
   demo:
   tms:
@@ -96,7 +96,7 @@ def writeCombinedMap(layers):
     md:
       title: Hafjall ehf. MapProxy
       abstract: WMS/WMTS cache owned and operated by Hafjall ehf.
-  
+
 layers:
 """)
   for layer in layers:
@@ -104,11 +104,11 @@ layers:
     extent = layer[1]
     shapefile = layer[2]
     layername = path.basename(path.normpath(tiledir))
-    
+
     cachefile.write("  - name: "+layername+"\n")
     cachefile.write("    title: "+layername+"\n")
     cachefile.write("    sources: ["+layername+"_cache]\n")
-  
+
   cachefile.write("""  - name: LMI_Kort
     title: Base map, Landmaelingar Islands
     sources: [lmi_cache]
@@ -118,39 +118,39 @@ layers:
     sources: [lmi_dem_cache]
 """)
   cachefile.write("caches:\n")
-  
+
   for layer in layers:
     tiledir = layer[0]
     extent = layer[1]
     shapefile = layer[2]
     layername = path.basename(path.normpath(tiledir))
-    
+
     cachefile.write("  "+layername+"_cache:\n")
-    cachefile.write("    grids: [GLOBAL_WEBMERCATOR]\n")
+    cachefile.write("    grids: [webmercator]\n")
     cachefile.write("    sources: ["+layername+"_wms]\n")
 #    cachefile.write("    coverage:\n")
 #    cachefile.write("       bbox: [-2056587.0588, 9101168.695, -1146597.0, 9844459.7153]\n")
 #    cachefile.write("       bbox_srs: EPSG:3857\n")
-  
+
   cachefile.write("""  lmi_cache:
-    grids: [GLOBAL_WEBMERCATOR]
+    grids: [webmercator]
     sources: [lmi_wms]
 """)
-  
+
   cachefile.write("""  lmi_dem_cache:
     format: image/jpeg
-    grids: [GLOBAL_WEBMERCATOR]
+    grids: [webmercator]
     sources: [lmi_dem_wms]
 """)
-  
+
   cachefile.write("sources:\n")
-  
+
   for layer in layers:
     tiledir = layer[0]
     extent = layer[1]
     shapefile = layer[2]
     layername = path.basename(path.normpath(tiledir))
-    
+
     cachefile.write("  "+layername+"_wms:\n")
     cachefile.write("    type: mapserver\n")
     cachefile.write("    req:\n")
@@ -163,7 +163,7 @@ layers:
     cachefile.write("    coverage:\n")
     cachefile.write("      bbox: [%s,%s,%s, %s]\n" % (extent[0], extent[2], extent[1], extent[3]))
     cachefile.write("      srs: 'EPSG:3857'\n")
-  
+
   cachefile.write("""
   lmi_wms:
     type: wms
@@ -174,7 +174,7 @@ layers:
       version: 1.3.0
     http:
       ssl_no_cert_checks: true
-    
+
   lmi_dem_wms:
     type: wms
     supported_srs: [EPSG:3057]
@@ -185,10 +185,12 @@ layers:
       version: 1.3.0
     http:
       ssl_no_cert_checks: true
-    
+
 grids:
   webmercator:
     base: GLOBAL_WEBMERCATOR
+    num_levels: 22
+    origin: ul
   webmercator_AN:
     base: GLOBAL_WEBMERCATOR
     bbox: [-1814636.9, 9372199.8, -1813440.3, 9373544.1]
@@ -201,7 +203,7 @@ grids:
     base: GLOBAL_WEBMERCATOR
     bbox: [-2056587.0588, 9101168.695, -1146597.0, 9844459.7153]
     bbox_srs: EPSG:3857
-  
-globals:  
+
+globals:
 """)
   cachefile.close()
